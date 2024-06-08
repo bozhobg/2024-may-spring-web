e8fbf9fc7f2e37c996894703566a3084416c227b
Bozho Georgiev <bozhobg@gmail.com>
2024-06-08 20:18:55 +0300

    login and register model redirection with form filled data.

--- pathfinder/src/main/java/bg/softuni/pathfinder/web/UserController.java
+++ pathfinder/src/main/java/bg/softuni/pathfinder/web/UserController.java
@@ -1,8 +1,10 @@
 package bg.softuni.pathfinder.web;
 
+import bg.softuni.pathfinder.service.dto.UserLoginDTO;
 import bg.softuni.pathfinder.service.dto.UserRegisterDTO;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,15 +17,48 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class UserController {
 
     @GetMapping("login")
-    public ModelAndView getLogin() {
+    public ModelAndView getLogin(Model bindingModel) {
+        String dataKey = "loginData";
+        Object dataValue = bindingModel.getAttribute("loginData");
 
-        return new ModelAndView("login");
+        ModelAndView mnv = new ModelAndView("login");
+        mnv.addObject(dataKey, dataValue == null ? new UserLoginDTO() : dataValue);
+
+        return mnv;
     }
 
-    @GetMapping("register")
-    public ModelAndView getRegister() {
+    @PostMapping("login")
+    public ModelAndView doLogin(
+            @Valid UserLoginDTO loginData,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes
+    ) {
+//        TODO: implement login logic and redirect logged-in user to homepage
+        ModelAndView mnv = new ModelAndView();
 
-        return new ModelAndView("register");
+        if (bindingResult.hasErrors()) {
+
+            redirectAttributes.addFlashAttribute("loginData", loginData);
+            mnv.setViewName("redirect:/users/login");
+
+            return mnv;
+        }
+
+        mnv.setViewName("redirect:/");
+
+        return mnv;
+    }
+
+
+    @GetMapping("register")
+    public ModelAndView getRegister(Model bindingModel) {
+        String dataKey = "registerData";
+        Object dataValue = bindingModel.getAttribute(dataKey);
+
+        ModelAndView mnv = new ModelAndView("register");
+        mnv.addObject(dataKey, dataValue == null ? new UserRegisterDTO() : dataValue);
+
+        return mnv;
     }
 
     @PostMapping("register")
@@ -35,8 +70,10 @@ public class UserController {
         ModelAndView mnv = new ModelAndView();
 
         if (bindingResult.hasErrors()) {
-            redirectAttributes.addFlashAttribute("registerDTO", userRegisterDTO);
-            mnv.setViewName("redirect:/register");
+            redirectAttributes.addFlashAttribute("registerData", userRegisterDTO);
+//            TODO: Error handling
+            mnv.setViewName("redirect:/users/register");
+            return mnv;
         }
 
         mnv.setViewName("redirect:/users/login");
