a786483cc64ec3bf73d93863b9cc86e66711d113
Bozho Georgiev <bozhobg@gmail.com>
2024-06-28 19:36:00 +0300

    MOBILELE: External data.sql to populate DB tables - brands, models, offers (borrowed from last year colleague in course). DB init in CommandLineRunner for users and roles (to be ran first)..

--- mobilele/src/main/java/bg/softuni/mobilele/config/DbInit.java
+++ mobilele/src/main/java/bg/softuni/mobilele/config/DbInit.java
@@ -1,25 +1,46 @@
 package bg.softuni.mobilele.config;
 
+import bg.softuni.mobilele.model.entity.User;
 import bg.softuni.mobilele.model.entity.UserRole;
 import bg.softuni.mobilele.model.enums.Role;
 import bg.softuni.mobilele.repository.RoleRepository;
+import bg.softuni.mobilele.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Component
 public class DbInit implements CommandLineRunner {
+//    TODO: move init logic in services
+
+    private final static int USER_COUNT_BOUND = 8;
+    private final static int ADMIN_COUNT_BOUND = 4;
 
     private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public DbInit (RoleRepository roleRepository) {
+    public DbInit(
+            RoleRepository roleRepository,
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder
+    ) {
         this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void run(String... args) throws Exception {
         this.initRoles();
+        this.initUsers();
     }
 
     private void initRoles() {
@@ -30,5 +51,42 @@ public class DbInit implements CommandLineRunner {
         for (Role role : Role.values()) {
             this.roleRepository.save(new UserRole().setName(role));
         }
+    }
+
+    private void initUsers() {
+        if (this.userRepository.count() > 0) return;
+
+        String userBase = "user";
+        String adminBase = "admin";
+
+        Random random = new Random();
+        int userCount = random.nextInt(USER_COUNT_BOUND);
+        int adminCount = random.nextInt(ADMIN_COUNT_BOUND);
+
+        List<User> users = this.generateUserListByRole(userBase, userCount, Role.USER);
+        this.userRepository.saveAll(users);
+
+        List<User> admins = this.generateUserListByRole(adminBase, adminCount, Role.ADMIN);
+        this.userRepository.saveAll(admins);
+    }
+
+    private List<User> generateUserListByRole(String nameBase, int count, Role role) {
+        List<User> users = new ArrayList<>();
+
+        for (int i = 0; i <= count; i++) {
+
+            int seq = i + 1;
+
+            users.add(new User()
+                    .setUsername(nameBase + seq)
+                    .setFirstName(nameBase + seq)
+                    .setLastName(nameBase + "ov" + seq)
+                    .setPassword(this.passwordEncoder.encode(nameBase + seq))
+                    .setCreated(Instant.now())
+                    .setRole(this.roleRepository.findByName(role))
+                    .setActive(true));
+        }
+
+        return users;
     }
 }
--- mobilele/src/main/java/bg/softuni/mobilele/repository/RoleRepository.java
+++ mobilele/src/main/java/bg/softuni/mobilele/repository/RoleRepository.java
@@ -1,10 +1,11 @@
 package bg.softuni.mobilele.repository;
 
 import bg.softuni.mobilele.model.entity.UserRole;
+import bg.softuni.mobilele.model.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<UserRole, Long> {
-
+    UserRole findByName(Role role);
 }
